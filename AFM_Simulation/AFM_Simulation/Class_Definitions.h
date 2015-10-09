4e3ff2cb1e74367353496a3ba1e59c3d76f6e14f
Adam Dempsey <adamdkayaker@gmail.com>
2015-10-09 17:18:07 +0100

    [impl] Add 3d lattice generator for multiple atoms in a basis and multiple offset layers.

--- AFM_Simulation/AFM_Simulation/Class_Definitions.h
+++ AFM_Simulation/AFM_Simulation/Class_Definitions.h
@@ -24,6 +24,11 @@ public:
 		cout << r[0] << " " << r[1] << " " << r[2] << " " << radius <<" "<< m << endl;
 	}
 
+	void Print(int lattice, int atom)
+	{
+		cout << r[0] << " " << r[1] << " " << r[2] << " " << radius << " " << m << " " <<lattice<<" "<<atom<< endl;
+	}
+
 };
 
 class Unit_Cell
@@ -31,7 +36,6 @@ class Unit_Cell
 public:
 	class Lattice;
 	Atom* first_atom;
-	Atom* final_atom;
 	Atom* temp_atom;
 	Unit_Cell* next;
 	double r[3];		//position vector of unit cell 
@@ -39,7 +43,6 @@ public:
 	Unit_Cell()		//Constructor
 	{
 		first_atom = NULL;
-		final_atom = NULL;
 		temp_atom = NULL;
 		next = NULL;
 		r[0] = 0.0;
@@ -50,7 +53,6 @@ public:
 	Unit_Cell(double r0_in, double r1_in, double r2_in)
 	{
 		first_atom = NULL;
-		final_atom = NULL;
 		temp_atom = NULL;
 		next = NULL;
 		r[0] = r0_in;
@@ -61,42 +63,31 @@ public:
 
 	void Add_Atom(Atom Atom_in)
 	{
-		//temp_atom = first_atom;
-		//first_atom = new Atom(Atom_in.r[0], Atom_in.r[1], Atom_in.r[2], Atom_in.radius, Atom_in.m);
-		//first_atom->Next = temp_atom;
 		if (first_atom == NULL)
 		{
-			first_atom = new Atom(Atom_in.r[0], Atom_in.r[1], Atom_in.r[2], Atom_in.radius, Atom_in.m);
+			first_atom = new Atom(Atom_in.r[0] + r[0], Atom_in.r[1] + r[1], Atom_in.r[2] + r[2], Atom_in.radius, Atom_in.m);
 		}
 		else
 		{
-			first_atom->Next = new Atom(Atom_in.r[0], Atom_in.r[1], Atom_in.r[2], Atom_in.radius, Atom_in.m);
+			first_atom->Next = new Atom(Atom_in.r[0] + r[0], Atom_in.r[1] + r[1], Atom_in.r[2] + r[2], Atom_in.radius, Atom_in.m);
 		}
 		//Give atoms absolute coordinates by adding displacement of current cell from the origin.
-		first_atom->r[0] += this->r[0];
-		first_atom->r[1] += this->r[1];
-		first_atom->r[2] += this->r[2];
-		cout << first_atom->r[0] << " " << first_atom->r[1] << " " << first_atom->r[2] <<  endl;
+
 	}
 
 	void Add_Atom(Atom* Atom_in)
 	{
-		//temp_atom = first_atom;
-		//first_atom = new Atom(Atom_in->r[0], Atom_in->r[1], Atom_in->r[2], Atom_in->radius, Atom_in->m);
-		//first_atom->Next = temp_atom;
 		if (first_atom == NULL)
 		{
-			first_atom = new Atom(Atom_in->r[0], Atom_in->r[1], Atom_in->r[2], Atom_in->radius, Atom_in->m);
+			first_atom = new Atom(Atom_in->r[0] + r[0], Atom_in->r[1] + r[1], Atom_in->r[2] + r[2], Atom_in->radius, Atom_in->m);
 		}
 		else
 		{
-			first_atom->Next = new Atom(Atom_in->r[0], Atom_in->r[1], Atom_in->r[2], Atom_in->radius, Atom_in->m);
+			first_atom->Next = new Atom(Atom_in->r[0] + r[0], Atom_in->r[1] + r[1], Atom_in->r[2] + r[2], Atom_in->radius, Atom_in->m);
 		}
 
 		//Give atoms absolute coordinates by adding displacement of current cell from the origin.
-		first_atom->r[0] += this->r[0];
-		first_atom->r[1] += this->r[1];
-		first_atom->r[2] += this->r[2];
+
 	}
 
 	void Print()
@@ -110,9 +101,18 @@ public:
 		first_atom = temp_atom;
 	}
 
-	
-
-
+	void Print(int lattice)
+	{
+		int atom=0;
+		temp_atom = first_atom;
+		while (first_atom != NULL)
+		{
+			first_atom->Print(lattice, atom);
+			first_atom = first_atom->Next;
+			atom++;
+		}
+		first_atom = temp_atom;
+	}
 };
 
 class Lattice
@@ -121,34 +121,17 @@ class Lattice
 public:
 
 	Unit_Cell* first_cell;
-	Unit_Cell* final_cell;
 	Unit_Cell* temp_cell;
-	Unit_Cell* temp_cell_2;
+	Unit_Cell* print_cell;
 	Lattice* next;
 	double a1[3];	//lattice vector 1
 	double a2[3];	//lattice vector 2
+	double a3[3];	//lattice vector 2
+	double offset[3] = { 0,0,0 };
 
-	Lattice(double a1_in[3], double a2_in[3])	//Constructor a1 and a2 are in nm, Phi is in degrees
+	Lattice(double a1_in[3], double a2_in[3], double a3_in[3])	//Constructor a1 and a2 are in nm, Phi is in degrees
 	{
 		first_cell = NULL;
-		final_cell = NULL;
-		temp_cell = NULL;
-		temp_cell_2 = NULL;
-		next = NULL;
-
-		a1[0] = a1_in[0];
-		a1[1] = a1_in[1];
-		a1[2] = a1_in[2];
-
-		a2[0] = a2_in[0];
-		a2[1] = a2_in[1];
-		a2[2] = a2_in[2];
-	}
-
-	void Generate_Lattice(double a1_in[3], double a2_in[3])
-	{
-		first_cell = NULL;
-		final_cell = NULL;
 		temp_cell = NULL;
 		next = NULL;
 
@@ -159,22 +142,54 @@ public:
 		a2[0] = a2_in[0];
 		a2[1] = a2_in[1];
 		a2[2] = a2_in[2];
+
+		a3[0] = a3_in[0];
+		a3[1] = a3_in[1];
+		a3[2] = a3_in[2];
 	}
+
+
+	Lattice(double a1_in[3], double a2_in[3], double a3_in[3], double offset_in[3])	//Constructor a1 and a2 are in nm, Phi is in degrees
+	{
+		first_cell = NULL;
+		temp_cell = NULL;
+		next = NULL;
+
+		a1[0] = a1_in[0];
+		a1[1] = a1_in[1];
+		a1[2] = a1_in[2];
+
+		a2[0] = a2_in[0];
+		a2[1] = a2_in[1];
+		a2[2] = a2_in[2];
+
+		a3[0] = a3_in[0];
+		a3[1] = a3_in[1];
+		a3[2] = a3_in[2];
+
+		offset[0] = offset_in[0];
+		offset[1] = offset_in[1];
+		offset[2] = offset_in[2];
+	}
+
 	
-	void Tile_Space(double x_max, double y_max)
-	{
-		while (first_cell->r[1] <= (y_max))
+	void Tile_Space(int a1_cells, int a2_cells, int a3_cells)
+	{	
+		for (double k = 0; k < a3_cells; k++)
 		{
-			while (first_cell->r[0] <= (x_max ))
+			for (double j = 0; j < a2_cells; j++)
 			{
-				temp_cell = first_cell;
-				first_cell = new Unit_Cell((temp_cell->r[0] + a1[0]), (temp_cell->r[1] + a1[1]), (temp_cell->r[2] + a1[2]));
-				first_cell->next = temp_cell;
+				for (double i = 0; i < a1_cells; i++)
+				{
+					if (i == 0 && j == 0 && k == 0)
+					{ 
+						i++;
+					}
+					temp_cell = first_cell;
+					first_cell= new Unit_Cell((a1[0] *(i+offset[0]) + a2[0] * (j + offset[1]) + a3[0] * (k + offset[2])), (a1[1] * (i + offset[0]) + a2[1] * (j + offset[1]) + a3[1] * (k + offset[2])) , (a1[2] * (i + offset[0]) + a2[2] * (j + offset[1]) + a3[2] * (k + offset[2])));
+					first_cell->next = temp_cell;
+				}
 			}
-			first_cell->r[0] = 0;
-			temp_cell = first_cell;
-			first_cell = new Unit_Cell((temp_cell->r[0] + a2[0]), (temp_cell->r[1] + a2[1]), (temp_cell->r[2] + a2[2]));
-			first_cell->next = temp_cell;
 		}
 	}
 	
@@ -183,10 +198,21 @@ public:
 	{
 		temp_cell = first_cell;
 		Cell_in->temp_atom = Cell_in->first_atom;
+
+		while (Cell_in->first_atom != NULL)
+		{
+			Cell_in->first_atom->r[0] = ((Cell_in->first_atom->r[0] * a1[0]) + (Cell_in->first_atom->r[1] * a2[0]) + (Cell_in->first_atom->r[2] * a3[0]));
+			Cell_in->first_atom->r[1] = ((Cell_in->first_atom->r[0] * a1[1]) + (Cell_in->first_atom->r[1] * a2[1]) + (Cell_in->first_atom->r[2] * a3[1]));
+			Cell_in->first_atom->r[2] = ((Cell_in->first_atom->r[0] * a1[2]) + (Cell_in->first_atom->r[1] * a2[2]) + (Cell_in->first_atom->r[2] * a3[2]));
+			Cell_in->first_atom = Cell_in->first_atom->Next;
+		}
+
+		Cell_in->first_atom = Cell_in->temp_atom;
+
 		while (first_cell->next != NULL)
 		{
 			while (Cell_in->first_atom != NULL)
-			{
+			{		
 				first_cell->Add_Atom(Cell_in->first_atom);
 				Cell_in->first_atom = Cell_in->first_atom->Next;
 			}
@@ -198,23 +224,39 @@ public:
 
 	void Print()
 	{
-		temp_cell = first_cell;
+		print_cell = first_cell;
 		while (first_cell != NULL)
 		{
 			first_cell->Print();
 			first_cell = first_cell->next;
 		}
-		first_cell = temp_cell;
+		first_cell = print_cell;
 	}
+
+	void Print(int lattice)
+	{
+		print_cell = first_cell;
+		while (first_cell != NULL)
+		{
+			first_cell->Print(lattice);
+			first_cell = first_cell->next;
+		}
+		first_cell = print_cell;
+	}
+
+	
+
 };
 
 class Surface
 {
 public:
 	Lattice* first_Lattice;
+	Lattice* temp_Lattice;
 	Atom* first_Atom;
 	Atom* temp_Atom;
 	Atom* temp;
+	int layer;
 
 	Surface()		//Constructor
 	{
@@ -223,22 +265,48 @@ public:
 		temp = NULL;
 	}
 
-	void Generate_Surface(Lattice* Lattice_in, Unit_Cell* Cell_in, double x_max, double y_max)
+	Surface(Lattice* Lattice_in, Unit_Cell* Cell_in, int a1_cells, int	a2_cells, int a3_cells)
 	{
 		first_Lattice = Lattice_in;
-		first_Lattice->first_cell = Cell_in;
-		first_Lattice->Tile_Space(x_max, y_max);
-		first_Lattice->Fill_Tiled_Space(Cell_in);
+		temp_Lattice = first_Lattice;
+		while (first_Lattice != NULL)
+		{
+			first_Lattice->first_cell = Cell_in;
+			first_Lattice->Tile_Space(a1_cells, a2_cells, a3_cells);
+			first_Lattice->Fill_Tiled_Space(Cell_in);
+			first_Lattice = first_Lattice->next;
+		}
+		first_Lattice = temp_Lattice;
 		first_Atom = first_Lattice->first_cell->first_atom;
+		temp = NULL;
 	}
+
 
 	void Print()
 	{
-	
-		first_Lattice->Print();
-
+		temp_Lattice = first_Lattice;
+		while (first_Lattice != NULL)
+		{
+			first_Lattice->Print();
+			first_Lattice = first_Lattice->next;
+		}
+		first_Lattice = temp_Lattice;
 	}
 
+	void Print_Lattice_Atom()
+	{
+		int lattice = 0;
+		temp_Lattice = first_Lattice;
+		while (first_Lattice != NULL)
+		{
+			first_Lattice->Print(lattice);
+			first_Lattice = first_Lattice->next;
+			lattice++;
+		}
+		first_Lattice = temp_Lattice;
+	}
+
+	
 };
 
 #endif
--- AFM_Simulation/AFM_Simulation/Main.cpp
+++ AFM_Simulation/AFM_Simulation/Main.cpp
@@ -8,31 +8,45 @@ using namespace std;
 
 #include "Class_Definitions.h"  //Header File containing class definitions
 
-
-int	main()
+void PrintToFile(Surface* surface)
 {
-	double x_max = 20; //20nm x extent
-	double y_max = 20; //20nm y extent
-	double a1[3] = { 1, 0, 0 };
-	double a2[3] = { 0, 1, 0 };
-
-
-	Atom Carbon(0.0, 0.0, 0.0, 1.0, 1.0);  //x,y,z,r,m;
-	Unit_Cell* simple_cell = new Unit_Cell();
-	simple_cell->Add_Atom(Carbon);
-	Lattice* Square = new Lattice(a1, a2);
-	Surface* surface = new Surface();
-
-	surface->Generate_Surface(Square, simple_cell, x_max, y_max);
-
 	std::ofstream out("AFM.dat");
 	std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
 	std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
 
-	surface->Print();
-
+	surface->Print_Lattice_Atom();
 
 	std::cout.rdbuf(coutbuf); //reset to standard output again
+}
+
+int	main()
+{
+	int a1_cells = 10;
+	int	a2_cells = 10;
+	int a3_cells = 5;
+	double a1[3] = {1,0,0};
+	double a2[3] = {0,1,0};
+	double a3[3] = {0,0,1};
+	double offset[3] = { 0.5, 0.0, 0.5 };
+	
+	Atom Carbon1(0, 0.0, 0.0, 1.0, 1.0);  //x,y,z,r,m;
+	Atom Carbon2(0.5, 0.5, 0.0, 1.0, 1.0);  //x,y,z,r,m;
+
+
+	Unit_Cell* simple_cell = new Unit_Cell();
+
+	simple_cell->Add_Atom(Carbon1);
+	simple_cell->Add_Atom(Carbon2);
+
+	Lattice* Square = new Lattice(a1, a2, a3);
+	Lattice* Square2 = new Lattice(a1, a2, a3, offset);
+	//Square->next = Square2;
+
+	Surface* surface = new Surface(Square, simple_cell, a1_cells, a2_cells, a3_cells);
+
+	PrintToFile(surface);
+
+
 
 	return(0);
 }
